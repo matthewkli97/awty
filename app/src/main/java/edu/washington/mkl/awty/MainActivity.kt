a88233d20fd510d1fbcca9c00ef619a37c367683
Matthew Li <matthewli@MacBookPro.local>
2018-05-12 23:26:18 -0700

    add text functionality

--- app/src/main/java/edu/washington/mkl/awty/MainActivity.kt
+++ app/src/main/java/edu/washington/mkl/awty/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.washington.mkl.awty
 
+import android.Manifest
 import android.app.IntentService
 import android.content.Context
 import android.content.Intent
@@ -12,25 +13,31 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import android.support.v4.app.ActivityCompat
+import android.content.pm.PackageManager
+import android.support.v4.content.ContextCompat
+import android.telephony.SmsManager
+
 
 class MainActivity : AppCompatActivity() {
 
+
+    private val MY_PERMISSIONS_REQUEST_SEND_SMS = 0
     var time:Int = 0
     var text:String = ""
     var phone:String = ""
-
+    var running = false
+    lateinit var textIntent:Intent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        textIntent = Intent(this, MyService::class.java)
         val btn_start = findViewById(R.id.btn_start) as Button
         val et_time = findViewById(R.id.et_time) as EditText
         val et_phone = findViewById(R.id.et_phone) as EditText
         val et_text = findViewById(R.id.et_text) as EditText
-
-        val intent = Intent(this, MyService::class.java)
-        var running = false
 
         et_phone.addTextChangedListener(object  : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
@@ -84,16 +91,11 @@ class MainActivity : AppCompatActivity() {
         btn_start.setOnClickListener { view ->
 
             if(running) {
-                stopService(intent)
+                stopService(textIntent)
                 running = false
                 btn_start.text = "Start"
             } else {
-                val message = "(" + phone.substring(0, 3) + ")" + phone.substring(3,6) + " - " + phone.substring(6) + " : " + text
-                intent.putExtra("text", message)
-                intent.putExtra("pause", time)
-                startService(intent)
-                running = true
-                btn_start.text = "Stop"
+                sendSMSMessage()
             }
         }
         updateInputs(running)
@@ -116,6 +118,7 @@ class MainActivity : AppCompatActivity() {
             te_phone.isEnabled = true
             te_time.isEnabled = true
 
+            btn_start.isEnabled = true
             if(time > 0 && text.length > 0 && phone.length == 10) {
                 btn_start.isEnabled = true
             } else {
@@ -124,6 +127,51 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    protected fun sendSMSMessage() {
+        val btn_start = findViewById(R.id.btn_start) as Button
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                            Manifest.permission.SEND_SMS)) {
+                textIntent.putExtra("text", text)
+                textIntent.putExtra("text", text)
+                        .putExtra("pause", time)
+                textIntent.putExtra("text", text)
+                        .putExtra("phone", phone)
+                startService(textIntent)
+                running = true
+                btn_start.text = "Stop"
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.SEND_SMS),
+                        MY_PERMISSIONS_REQUEST_SEND_SMS)
+            }
+        } else {
+            textIntent.putExtra("text", text)
+            textIntent.putExtra("text", text)
+                    .putExtra("pause", time)
+            textIntent.putExtra("text", text)
+                    .putExtra("phone", phone)
+            startService(textIntent)
+            running = true
+            btn_start.text = "Stop"
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_SEND_SMS -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    Toast.makeText(applicationContext,
+                            "SMS faild, please try again.", Toast.LENGTH_LONG).show()
+                    return
+                }
+            }
+        }
+
+    }
 }
 
 class MyService : IntentService("MyService") {
@@ -138,13 +186,18 @@ class MyService : IntentService("MyService") {
 
     override fun onHandleIntent(intent: Intent?) {
         Log.i("MyService", "Thread Start")
-        val toastText = intent!!.getStringExtra("text")
+        val text = intent!!.getStringExtra("text")
+        val phoneNumber = intent!!.getStringExtra("phone")
         val minutes = intent!!.getIntExtra("pause", 1)
 
         while(!stopped) {
             try {
                 Log.i("Thread", "hello")
-                mHandler.post(DisplayToast(this, toastText))
+                mHandler.post(DisplayToast(this, text))
+
+                val smsManager = SmsManager.getDefault()
+                smsManager.sendTextMessage(phoneNumber, null, text, null, null)
+
                 Thread.sleep((60000 * minutes).toLong())
             } catch (e: InterruptedException) {
                 Thread.currentThread().interrupt()
